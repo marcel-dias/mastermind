bf14c2f9c303e2464a4b2f4f1e3f057822fa485a
Marcel Dias <marceldiass@gmail.com>
2016-05-21 10:13:59 -0300

    Initial commit - mastermind project structure

---
+++ src/main/java/com/marceldias/MastermindApplication.java
@@ -0,0 +1,12 @@
+package com.marceldias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastermindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastermindApplication.class, args);
+	}
+}
