4d0e727396c1fff1e48885695cae590f5d085e02
Marcel Dias <marceldiass@gmail.com>
2016-05-22 04:33:35 -0300

    fix start script to fit into docker container

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-export MONGODB_URI="mongodb://192.168.99.100:27017/mastermind"
-java -jar target/mastermind-0.0.1-SNAPSHOT.jar --server.port=8080 --spring.data.mongodb.uri=$MONGODB_URI
+if [ "x$MONGODB_URI" = "x" ]; then
+    MONGODB_URI="mongodb://192.168.99.100:27017/mastermind"
+fi
+if [ "x$SERVER_PORT" = "x" ]; then
+    SERVER_PORT="8080"
+fi
+if [ "x$JAR_PATH" = "x" ]; then
+    JAR_PATH="target/mastermind.jar"
+fi
+
+java -jar $JAR_PATH --server.port=$SERVER_PORT --spring.data.mongodb.uri=$MONGODB_URI
